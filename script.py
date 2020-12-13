7c892ce0b86c50fcc1e8a2e567b0434b7852974a
Ch0l0 <73145048+ch0l0hatwhite@users.noreply.github.com>
2020-12-13 00:01:10 -0600

    Add files via upload

---
+++ script.py
@@ -0,0 +1,61 @@
+print('...######...####...####....#######....##........#######.')
+print('.##.....##...##.....##....##.....##...##.......##.....##')
+print('.##..........##.....##....##.....##...##.......##.....##')
+print('.##..........#########....##.....##...##.......##.....##')
+print('.##..........#########....##.....##...##.......##.....##')
+print('.##..........##.....##....##.....##...##.......##.....##')
+print('.##.....##...##.....##....##.....##...##.......##.....##')
+print('...######...####...####....#######....#######...#######.')
+
+import smtplib
+
+corre= smtplib.SMTP_SSL('smtp.gmail.com',465)
+
+correo = input('Tu correo > ')
+
+
+print('')
+
+print('')
+
+
+contraseña = input('Tu contraseña  > ')
+
+print('')
+print('')
+
+corre.login(correo , contraseña)
+
+
+
+
+mensaje = input('Tu mensaje > ')
+
+
+print('')
+print('')
+
+destinatario = input('destinatario > ')
+
+
+
+
+corre.sendmail
+(correo , destinatario , mensaje)
+
+corre.quit()
+
+
+print('')
+print('')
+
+
+
+print('Tu mensaje ah sido enviado a ' +
+destinatario)
+
+
+print('')
+
+print('')
+
